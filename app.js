387de8763a6e7e0d68cbf16d9a8546adf93dc064
Tyler Smith <tylsmith@gmail.com>
2014-10-21 11:38:53 -0700

    added pop philo essay

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.get('/truths', function(req, res){
   res.render('truths')
 })
 
+app.get('/essays', function(req, res){
+  res.render('essays')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
